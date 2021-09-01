6093c64101851882b734febbb6c73fbbbdf70ba6
borzo-m <48838680+borzo-m@users.noreply.github.com>
2021-09-01 19:51:44 +0200

    End of chapter 2

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,24 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Max'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Max'},
+            'body': 'It is so cold in september'
+        }
+    ]
+
+    return render_template('index.html', title='Home', user=user, posts=posts)
